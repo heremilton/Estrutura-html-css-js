98a8b856ce45a856e8a71d8e5b0c9d7021f96568
heremilton <107191128+heremilton@users.noreply.github.com>
2024-11-18 21:04:20 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ function adicionarCuriosidade() {
     const curiosidades = [
         "Java tem uma forte comunidade open-source.",
         "Java é usado em muitas empresas de grande porte.",
-        "É a linguagem preferida para o desenvolvimento Android."
+        "É a linguagem preferida para o desenvolvimento Android.","O nome Java foi inspirado no café que era consumido pelo time de desenvolvimento, que vinha da ilha de Java.","A linguagem Java tem apenas 52 palavras reservadas.","O Java foi criado pela Sun Microsystems e lançado em 1995.","O Java é uma linguagem orientada a objetos, o que permite criar programas modulares e código reutilizável.","O Minecraft foi programado em Java pela empresa Mojave.","O Java é utilizado para criar aplicativos móveis e Web, software empresarial, dispositivos de Internet das Coisas (IoT), jogos, Big Data, entre outros tipos.","A linguagem que ajudou a explorar Marte.","Mapeamento do genoma humano"
     ];
     const novaCuriosidade = curiosidades[Math.floor(Math.random() * curiosidades.length)];
     const lista = document.getElementById("lista-curiosidades");
